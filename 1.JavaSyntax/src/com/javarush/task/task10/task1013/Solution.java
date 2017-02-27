f41ac2c98d96be9c925c032eb7d2c72dc14279fe
Vitaly Chura <vcvitaly@yahoo.com>
2017-02-27 19:09:44 +0200

    2nd commit

--- 1.JavaSyntax/src/com/javarush/task/task10/task1013/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1013/Solution.java
@@ -9,21 +9,74 @@ public class Solution {
     }
 
     public static class Human {
-        String name;
-        int age;
-        int heigth;
-        int weight;
-        String address;
-        boolean sex;
+        private String name;
+        private int age;
+        private int heigth;
+        private int weight;
+        private String address;
+        private boolean sex;
 
         public Human(String name) {
             this.name = name;
         }
+
+        public Human(boolean sex) {
+            this.sex = sex;
+        }
+
         public Human(String name, int age) {
             this.name = name;
             this.age = age;
         }
-        public Human(String )
+
+        public Human(boolean sex, int age) {
+            this.sex = sex;
+            this.age = age;
+        }
+
+        public Human(boolean sex, int age, int heigth) {
+            this.sex = sex;
+            this.age = age;
+            this.heigth = heigth;
+        }
+
+        public Human(boolean sex, int age, int heigth, int weight) {
+            this.sex = sex;
+            this.age = age;
+            this.heigth = heigth;
+            this.weight = weight;
+        }
+
+        public Human(String name, String address, boolean sex) {
+            this.name = name;
+            this.address = address;
+            this.sex = sex;
+        }
+
+        public Human(String name, int age, int heigth, int weight, String address) {
+            this.name = name;
+            this.age = age;
+            this.heigth = heigth;
+            this.weight = weight;
+            this.address = address;
+        }
+
+        public Human(String name, int age, int heigth, int weight, boolean sex) {
+            this.name = name;
+            this.age = age;
+            this.heigth = heigth;
+            this.weight = weight;
+            this.sex = sex;
+        }
+
+        public Human(String name, int age, int heigth, int weight, String address, boolean sex) {
+            this.name = name;
+            this.age = age;
+            this.heigth = heigth;
+            this.weight = weight;
+            this.address = address;
+            this.sex = sex;
+        }
 
     }
 }
