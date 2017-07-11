953552f06f4d8f74328dd2905f2e9d8b380122e1
Vitaly Chura <vcvitaly@yahoo.com>
2017-07-11 09:29:22 +0300

    Updated task0903

--- 1.JavaSyntax/src/com/javarush/task/task09/task0903/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0903/Solution.java
@@ -12,29 +12,37 @@ public class Solution {
     public static int method1() {
         method2();
         StackTraceElement[] StackTraceElements = Thread.currentThread().getStackTrace();
+        //System.out.println(StackTraceElements[2].getLineNumber());
         return  StackTraceElements[2].getLineNumber();
     }
 
     public static int method2() {
         method3();
         StackTraceElement[] StackTraceElements = Thread.currentThread().getStackTrace();
+        //System.out.println(StackTraceElements[2].getLineNumber());
         return  StackTraceElements[2].getLineNumber();
     }
 
     public static int method3() {
         method4();
         StackTraceElement[] StackTraceElements = Thread.currentThread().getStackTrace();
+        //System.out.println(StackTraceElements[2].getLineNumber());
         return  StackTraceElements[2].getLineNumber();
     }
 
     public static int method4() {
         method5();
         StackTraceElement[] StackTraceElements = Thread.currentThread().getStackTrace();
+        //System.out.println(StackTraceElements[2].getLineNumber());
         return  StackTraceElements[2].getLineNumber();
     }
 
     public static int method5() {
         StackTraceElement[] StackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println(StackTraceElements[1].getLineNumber());
+        /*for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            System.out.println(element + " line number " + element.getLineNumber());
+        }*/
         return  StackTraceElements[2].getLineNumber();
     }
 }
