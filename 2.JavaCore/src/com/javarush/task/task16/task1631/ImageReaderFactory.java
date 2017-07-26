d94ff148e92b5ec067556af8651dff08efaa18ed
Vitaly Chura <vcvitaly@yahoo.com>
2017-07-26 18:24:39 +0300

    Task 1631 - replace case with if

--- 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -5,16 +5,14 @@ import com.javarush.task.task16.task1631.common.*;
 public class ImageReaderFactory {
     public static ImageReader getImageReader(ImageTypes imageType) {
         ImageReader reader;
-        switch (imageType) {
-            case BMP:
-                reader = new BmpReader();
-                break;
-            case JPG:
-                reader = new JpgReader();
-                break;
-            case PNG:
-                reader = new PngReader();
-                break;
+        if (imageType == ImageTypes.BMP) {
+            reader = new BmpReader();
+        } else if (imageType == ImageTypes.JPG) {
+            reader = new JpgReader();
+        } else if (imageType == ImageTypes.PNG) {
+            reader = new PngReader();
+        } else {
+            throw new IllegalArgumentException("Неизвестный тип картинки");
         }
         return reader;
     }
