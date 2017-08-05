a494197db351f2942381464692a40776893bed81
Vitaly Chura <vcvitaly@yahoo.com>
2017-08-05 13:59:44 +0300

    Level 17 - task 1715

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task17.task1714;
+
+/* 
+Comparable
+*/
+
+public class Beach implements Comparable<Beach> {
+    private String name;      //название
+    private float distance;   //расстояние
+    private int quality;    //качество
+
+    public Beach(String name, float distance, int quality) {
+        this.name = name;
+        this.distance = distance;
+        this.quality = quality;
+    }
+
+    @Override
+    public synchronized int compareTo(Beach o) {
+        int qualityCompare = Integer.compare(this.quality,o.quality);
+        if (qualityCompare != 0) {
+            return qualityCompare;
+        } else {
+            return Float.compare(this.distance,o.distance);
+        }
+    }
+
+    public synchronized String getName() {
+        return name;
+    }
+
+    public synchronized void setName(String name) {
+        this.name = name;
+    }
+
+    public synchronized float getDistance() {
+        return distance;
+    }
+
+    public synchronized void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public synchronized int getQuality() {
+        return quality;
+    }
+
+    public synchronized void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
