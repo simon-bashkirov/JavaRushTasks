6ead2c058dd03bb973c8a30e9ff32f1ed8e85c88
Vitaly Chura <vcvitaly@yahoo.com>
2017-10-16 19:00:10 +0300

    Level 21 - task2106 v1

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2106/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2106/Solution.java
@@ -28,17 +28,15 @@ public class Solution {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Solution)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
         Solution solution1 = (Solution) o;
 
-        if (Double.compare(solution1.aDouble, aDouble) != 0) return false;
         if (anInt != solution1.anInt) return false;
-        if (date != null ? !date.equals(solution1.date) : solution1.date == null) return false;
-        if (solution != null ? !solution.equals(solution1.solution) : solution1.solution == null) return false;
-        if (string != null ? !string.equals(solution1.string) : solution1.string == null) return false;
-
-        return true;
+        if (Double.compare(solution1.aDouble, aDouble) != 0) return false;
+        if (string != null ? !string.equals(solution1.string) : solution1.string != null) return false;
+        if (date != null ? !date.equals(solution1.date) : solution1.date != null) return false;
+        return solution != null ? solution.equals(solution1.solution) : solution1.solution == null;
     }
 
     @Override
