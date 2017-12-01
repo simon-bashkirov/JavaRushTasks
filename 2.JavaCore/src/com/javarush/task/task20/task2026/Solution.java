ad5c899647e478cca8e5ff497bae96b8c262a008
Vitaly Chura <vcvitaly@yahoo.com>
2017-12-01 22:25:51 +0200

    task2026 - in progress

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task20.task2026;
+
+/* 
+Алгоритмы-прямоугольники
+*/
+public class Solution {
+    public static void main(String[] args) {
+        byte[][] a = new byte[][]{
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1}
+        };
+        int count = getRectangleCount(a);
+        System.out.println("count = " + count + ". Должно быть 2");
+    }
+
+    public static int getRectangleCount(byte[][] a) {
+        int size = a.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+            }
+        }
+        return 0;
+    }
+}
