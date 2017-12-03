58e05ef79bc1e03e067f1ce9d175b37b71d63c2d
Vitaly Chura <vcvitaly@yahoo.com>
2017-12-03 14:14:21 +0200

    Level 23 - task2311 - Add constructors

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class Hippodrome {
     private List<Horse> horses;
 
+    public Hippodrome() {
+    }
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
     public static void main(String[] args) {
         
     }
