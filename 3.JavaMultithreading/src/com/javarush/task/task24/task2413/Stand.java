6001880f36463752af2a9dc6c5ad8c55b2e27338
Vitaly Chura <vcvitaly@yahoo.com>
2018-01-20 17:35:41 +0200

    Level 23 - task2413 - Арканоид(18)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -2,7 +2,7 @@ package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject {
     private double speed;
-    private double direction;
+    private double direction; // направление движения по оси x: 1 - вправо, -1 - влево, 0 - начальное значение, стоим на месте
 
     public Stand(double x, double y) {
         super(x, y, 3);
@@ -25,6 +25,14 @@ public class Stand extends BaseObject {
 
     @Override
     public void move() {
+        x += direction * speed;
+    }
 
+    public void moveLeft() {
+        direction = -1;
+    }
+
+    public void moveRight() {
+        direction = 1;
     }
 }
