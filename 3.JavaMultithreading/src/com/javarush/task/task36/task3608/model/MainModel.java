db66c26f0d9aaa94ce7abd9dfa827b3738ce9730
Vitaly Chura <vcvitaly@yahoo.com>
2018-02-03 19:24:53 +0200

    Squashed - Level 24 - tasks 3608 - MVC(9) done, the last task ...

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.*;
+
+//import java.util.ArrayList;
+import java.util.List;
+
+public class MainModel implements Model {
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(getAllUsers());
+        modelData.setDisplayDeletedUserList(false);
+    }
+
+    public void loadDeletedUsers() {
+        List<User> allDeletedUsers = userService.getAllDeletedUsers();
+        modelData.setUsers(allDeletedUsers);
+        modelData.setDisplayDeletedUserList(true);
+    }
+
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+    }
+
+    public void deleteUserById(long id) {
+        userService.deleteUser(id);
+        modelData.setUsers(getAllUsers());
+    }
+
+    private List<User> getAllUsers() {
+        List<User> result = userService.filterOnlyActiveUsers(userService.getUsersBetweenLevels(1, 100));
+        return result;
+    }
+
+    public void changeUserData(String name, long id, int level) {
+        User user = userService.createOrUpdateUser(name, id, level);
+        modelData.setUsers(getAllUsers());
+    }
+}
