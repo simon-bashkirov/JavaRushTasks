ef19d59c86e2561212dfef9729583e5b32e25282
Vitaly Chura <vcvitaly@yahoo.com>
2018-02-03 19:24:53 +0200

    Squashed - Level 24 - tasks 3608 - MVC(9) done, the last task ...

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task36.task3608;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.MainModel;
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
+import com.javarush.task.task36.task3608.view.EditUserView;
+//import com.javarush.task.task36.task3608.model.FakeModel;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        //initial code
+        Model model = new MainModel();
+        UsersView usersView = new UsersView();
+        EditUserView editUserView = new EditUserView();
+        Controller controller = new Controller();
+
+        usersView.setController(controller);
+        editUserView.setController(controller);
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
+
+        // my code
+        usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126L);
+        editUserView.fireEventUserDeleted(124L);
+        editUserView.fireEventUserChanged("Sidorov", 126L, 3);
+        usersView.fireEventShowDeletedUsers();
+    }
+}
