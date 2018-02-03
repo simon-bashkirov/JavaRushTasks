ad7adb2a03be7c3c98e6802fc4dee2295f20d726
Vitaly Chura <vcvitaly@yahoo.com>
2018-02-03 13:05:48 +0200

    Level 27 - tasks 2707 IP

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task27.task2707;
 
-/* 
+/*
 Определяем порядок захвата монитора
 */
 public class Solution {
@@ -13,20 +13,42 @@ public class Solution {
     }
 
     public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception {
-        /*synchronized (o1) {
-            solution.someMethodWithSynchronizedBlocks(o1,o2);
-            o1.wait();
-        }*/
-        synchronized (o2) {
-            solution.someMethodWithSynchronizedBlocks(o1,o2);
-            synchronized (o1) {
-                o2.wait();
-                o1.
+        final boolean[] b = {false};
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                solution.someMethodWithSynchronizedBlocks(o1,o2);
+                b[0] = true;
             }
+        });
+        t1.setDaemon(true);
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (o1) {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+            }
+        });
+        t2.setDaemon(true);
+
+        synchronized (o1) {
+            t1.start();
+            t2.start();
+            Thread.sleep(1);
+        }
+        synchronized (o1) {
+
         }
 
-        //
-        return false;
+
+        return t1.isAlive();
     }
 
     public static void main(String[] args) throws Exception {
