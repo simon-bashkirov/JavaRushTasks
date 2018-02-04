2ac935aaa7b27bd48a105a922489ed15ae9e30dc
Vitaly Chura <vcvitaly@yahoo.com>
2018-02-04 16:47:22 +0200

    Level 25 - task 2909 - Refactoring(2)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/Solution.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task29.task2909;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -14,8 +14,6 @@ public class Human {
 
     protected int[] size;
 
-    protected boolean isSoldier;
-
     public static final int FIRST = 1;
     public static final int SECOND = 2;
     public static final int THIRD = 3;
@@ -30,10 +28,10 @@ public class Human {
         return bloodGroup;
     }
 
-    public Human(boolean isSoldier) {
-        this.isSoldier = isSoldier;
-        this.id = nextId;
-        nextId++;
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = nextId++;
     }
 
     public int getAge() {
@@ -57,11 +55,6 @@ public class Human {
     }
 
     public void live() {
-        if (isSoldier)
-            fight();
-    }
-
-    public void fight() {
     }
 
     public int getId() {
