f648c44b5b79646d7202f6acd74229c8818a53db
Vitaly Chura <vcvitaly@yahoo.com>
2018-02-07 20:53:59 +0200

    Level 25 - task 2909 - Refactoring(9)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -11,19 +11,15 @@ public class Human implements Alive {
     protected String name;
     private List<Human> children = new ArrayList<>();
 
-    protected int[] size;
+    protected Size size;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
-    public int getBloodGroup() {
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
@@ -69,7 +65,7 @@ public class Human implements Alive {
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     public String getPosition() {
@@ -78,5 +74,16 @@ public class Human implements Alive {
 
     public void printData() {
         System.out.println(getPosition() + ": " + name);
+    }
+
+    public class Size {
+        public int height;
+        public int weight;
+
+        public Size(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+
     }
 }
