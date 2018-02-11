bc5d3738469957279f868aa4eb7bedffb4a09c5b
Vitaly Chura <vcvitaly@yahoo.com>
2018-02-11 18:33:31 +0200

    Level 26 - task 3008 - Chat(5-7)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,46 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("Please enter port number:");
+        int portNumber = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("Server is started");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (ConcurrentHashMap.Entry<String, Connection> entry : connectionMap.entrySet()) {
+            try {
+                entry.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println("Сообщение не было отправлено");
+            }
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
