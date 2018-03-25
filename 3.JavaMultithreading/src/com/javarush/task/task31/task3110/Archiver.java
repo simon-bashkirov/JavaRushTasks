bbd802f3369717c8ea2113e5d41193b91c1e8c64
Vitaly Chura <vcvitaly@yahoo.com>
2018-03-25 19:54:03 +0300

    Level 27 - task 110 - Archiver (8)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,20 +1,44 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Archiver {
 
     public static void main(String[] args) throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь к архиву");
+        /*ConsoleHelper.writeMessage("Введите полный путь к архиву");
         Path path = Paths.get(ConsoleHelper.readString());
         ZipFileManager zipFileManager = new ZipFileManager(path);
         ConsoleHelper.writeMessage("Введите полный путь к архивируемому файлу");
         Path source = Paths.get(ConsoleHelper.readString());
-        zipFileManager.createZip(source);
+        zipFileManager.createZip(source);*/
 
-        new ExitCommand().execute();
+        Operation operation = null;
+
+        while (true) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+                if (operation == Operation.EXIT)
+                    break;
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
+
+    }
+
+    public static Operation askOperation() throws Exception {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        for (Operation operation : Operation.values()) {
+            ConsoleHelper.writeMessage(operation.ordinal() + " - " + operation.getDescription());
+        }
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
@@ -1,10 +1,20 @@
 package com.javarush.task.task31.task3110;
 
 public enum Operation {
-    CREATE,
-    ADD,
-    REMOVE,
-    EXTRACT,
-    CONTENT,
-    EXIT
+    CREATE ("упаковать файлы в архив"),
+    ADD ("добавить файл в архив"),
+    REMOVE ("удалить файл из архива"),
+    EXTRACT ("распаковать архив"),
+    CONTENT ("просмотреть содержимое архива"),
+    EXIT ("выход");
+
+    private String description;
+
+    Operation(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
