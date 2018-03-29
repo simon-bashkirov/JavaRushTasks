9ee92bd82143e8b0865b2e9c9d9e0e35511869b1
Vitaly Chura <vcvitaly@yahoo.com>
2018-03-29 20:34:30 +0300

    Did some clean up and added unversioned pics

--- 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -15,42 +15,20 @@ public class Solution {
         String file2 = reader.readLine();
         String file3 = reader.readLine();
 
-        FileInputStream fileInputStream1 = new FileInputStream(file1);
-        FileOutputStream fileOutputStream2 = new FileOutputStream(file2);
-        FileOutputStream fileOutputStream3 = new FileOutputStream(file3);
-
-
-
-        if (fileInputStream1.available() > 0) {
-            int fileSizeBytes = fileInputStream1.available();
-            int toFile1, toFile2;
-            if (fileSizeBytes % 2 == 0) {
-                toFile1 = toFile2 = fileSizeBytes / 2;
-            } else {
-                toFile2 = fileSizeBytes / 2;
-                toFile1 = toFile2 + 1;
-            }
-            System.out.println(fileInputStream1.available());
-            byte[] buffer = new byte[toFile1];
-            int count = fileInputStream1.read(buffer);
-            fileOutputStream2.write(buffer, 0, count);
-            System.out.println(count);
-
-            buffer = new byte[toFile2];
-            count = fileInputStream1.read(buffer);
-            fileOutputStream3.write(buffer, 0, count);
-            System.out.println(count);
-
-
-        }
-        //System.out.println(countOfCommas);
-
-        fileInputStream1.close();
-        fileOutputStream2.close();
-        fileOutputStream3.close();
         reader.close();
 
-        //System.out.println(min_byte);
+        try (FileInputStream fileInputStream = new FileInputStream(file1);
+             FileOutputStream outputStream1 = new FileOutputStream(file2);
+             FileOutputStream outputStream2 = new FileOutputStream(file3)) {
+            if (fileInputStream.available() > 0) {
+                byte[] buffer = new byte[fileInputStream.available()];
+                int count = fileInputStream.read(buffer);
+                System.out.println(count);
+                outputStream1.write(buffer, 0, count/2+count%2);
+                outputStream2.write(buffer, count/2+count%2, count/2);
+
+            }
+        }
 
     }
 }
