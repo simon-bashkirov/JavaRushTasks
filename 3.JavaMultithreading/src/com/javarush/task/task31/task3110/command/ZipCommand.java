e5384111759b190e94e44412af7fd8c3767e1996
Vitaly Chura <vcvitaly@yahoo.com>
2018-04-03 14:56:24 +0300

    Level 27 - task 110 - Archiver (11)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,4 +1,15 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command {
+
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь к архиву");
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(ConsoleHelper.readString()));
+        return zipFileManager;
+    }
 }
