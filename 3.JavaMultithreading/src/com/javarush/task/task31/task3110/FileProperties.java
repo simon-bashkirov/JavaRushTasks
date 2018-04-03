3adce39b524792371c01bfe6f60950c929c7a678
Vitaly Chura <vcvitaly@yahoo.com>
2018-04-03 20:48:42 +0300

    Level 27 - task 110 - Archiver (13)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -13,9 +13,6 @@ public class FileProperties {
         this.compressionMethod = compressionMethod;
     }
 
-    public long getCompressionRatio() {
-        return 100 - (compressedSize * 100) / size;
-    }
 
     public String getName() {
         return name;
@@ -33,11 +30,26 @@ public class FileProperties {
         return compressionMethod;
     }
 
+    public long getCompressionRatio() {
+        // Вычисляем степень сжатия
+        return 100 - ((compressedSize * 100) / size);
+    }
+
     @Override
     public String toString() {
-        if (size > 0)
-            return String.format("%s %d Kb (%d Kb) сжатие: %d%%", name, size/1024, compressedSize/1024, getCompressionRatio());
-        else
-            return name;
+        // Строим красивую строку из свойств
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
+        if (size > 0) {
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
+        }
+
+        return builder.toString();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,13 +1,17 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
@@ -47,6 +51,30 @@ public class ZipFileManager {
                 throw new PathIsNotFoundException();
             }
         }
+    }
+
+    public List<FileProperties> getFilesList() throws Exception {
+        if (!Files.isRegularFile(zipFile))
+            throw new WrongZipFileException();
+
+        List<FileProperties> filePropertiesList = new ArrayList<>();
+
+        try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+            ZipEntry nextEntry;
+            while ( (nextEntry = zipInputStream.getNextEntry()) != null ) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                copyData(zipInputStream, byteArrayOutputStream);
+                String name = nextEntry.getName();
+                long size = nextEntry.getSize();
+                long compressedSize = nextEntry.getCompressedSize();
+                int compressionMethod = nextEntry.getMethod();
+                FileProperties fileProperties = new FileProperties(name, size, compressedSize, compressionMethod);
+                filePropertiesList.add(fileProperties);
+                zipInputStream.closeEntry();
+            }
+        }
+
+        return filePropertiesList;
     }
 
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
