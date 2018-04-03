d3867c5a19f51009b3394fd9eff5dc946c7b2950
Vitaly Chura <vcvitaly@yahoo.com>
2018-04-03 18:58:34 +0300

    Level 28 - task 3209 - HTML Editor(4)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task32.task3209;
+
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+    public void init() {
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        setVisible(true);
+    }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+}
