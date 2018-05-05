8f3e87f1b55fd7b8fe4168d357472e6ea2e75cbf
Vitaly Chura <vcvitaly@yahoo.com>
2018-05-05 18:13:41 +0300

    Level 29 - task 2712 - Restaurant(12)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task27.task2712.statistic;
+
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StatisticManager {
+    private static StatisticManager ourInstance = new StatisticManager();
+    private StatisticStorage statisticStorage = new StatisticStorage();
+
+    public static StatisticManager getInstance() {
+        return ourInstance;
+    }
+
+    private StatisticManager() {
+    }
+
+    public void register(EventDataRow data) {
+
+    }
+
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            for (EventType eventType : EventType.values()) {
+                storage.put(eventType, new ArrayList<EventDataRow>());
+            }
+        }
+    }
+}
