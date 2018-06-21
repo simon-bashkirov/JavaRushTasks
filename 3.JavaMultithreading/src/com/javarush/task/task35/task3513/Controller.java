7a380282176d5cf5eda10adf1d574074b0e8ca1c
Vitaly Chura <vcvitaly@yahoo.com>
2018-06-21 21:33:27 +0300

    Level 30 - task3513 - 2048(9)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,10 +1,45 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import static java.awt.event.KeyEvent.VK_ESCAPE;
 
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+    private static final int WINNING_TILE = 2048;
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == VK_ESCAPE)
+            resetGame();
+
+        if (!model.canMove())
+            view.isGameLost = true;
+
+        if (!view.isGameLost && !view.isGameWon)
+
+        switch (e.getKeyCode()) {
+            case (KeyEvent.VK_LEFT):
+                model.left();
+                break;
+            case (KeyEvent.VK_RIGHT):
+                model.right();
+                break;
+            case (KeyEvent.VK_UP):
+                model.up();
+                break;
+            case (KeyEvent.VK_DOWN):
+                model.down();
+                break;
+        }
+
+        if (model.maxTile == WINNING_TILE)
+            view.isGameWon = true;
+
+        view.repaint();
+    }
 
     public Controller(Model model) {
         this.model = model;
@@ -17,5 +52,11 @@ public class Controller extends KeyAdapter {
 
     public int getScore() {
         return model.score;
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameLost = view.isGameWon = false;
+        model.resetGameTiles();
     }
 }
