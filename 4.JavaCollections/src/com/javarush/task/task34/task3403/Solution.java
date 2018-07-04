0e5ce06657f6e08c4cc2474d31b6fbfba6f21c1d
Vitaly Chura <vcvitaly@yahoo.com>
2018-07-04 09:37:31 +0300

    Level 34 - ip

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task34.task3403;
+
+/* 
+Разложение на множители с помощью рекурсии
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.recursion(132);
+    }
+
+    public void recursion(int n) {
+
+    }
+}
