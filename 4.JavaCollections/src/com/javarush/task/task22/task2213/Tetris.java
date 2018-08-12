86ade30a6b4bbd47543326d7631b11242f3056bc
Vitaly Chura <vcvitaly@yahoo.com>
2018-08-12 13:45:45 +0300

    Level 31 - task 2213 - Tetris(7)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,10 +3,12 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Field field;
     private Figure figure;
+    static Tetris game;
 
 
     public static void main(String[] args) {
-
+        game = new Tetris();
+        game.run();
     }
 
     public Field getField() {
