f22e81e929b90e139d35b0ec5e8dde39e8799735
Vitaly Chura <vcvitaly@yahoo.com>
2018-09-22 11:38:41 +0300

    Level 33 - task 2515 - Space(12)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,17 +1,26 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Класс для бомбы.
+ */
 public class Bomb extends BaseObject {
     public Bomb(double x, double y) {
         super(x, y, 1);
     }
 
-    @Override
-    public void move() {
-        y++;
-    }
-
+    /**
+     * Отрисовываем себя на холсте.
+     */
     @Override
     public void draw(Canvas canvas) {
         canvas.setPoint(x, y, 'B');
+    }
+
+    /**
+     * Двигаем себя вниз на один ход.
+     */
+    @Override
+    public void move() {
+        y++;
     }
 }
